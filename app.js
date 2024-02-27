8d3d8a1959a33059615e4ab376d60861642ac328
eslyardon1010 <es_ardons@unicah.edu>
2024-02-27 13:54:44 -0600

    ft

--- app.js
+++ app.js
@@ -20,10 +20,7 @@ app.use('/barbero', barberoRoutes)
 app.use(generalRoutes)
 
 app.get('/', (req, res, next) => {
- app.use('/cliente', clienteRoutes)
-app.use('/admin', adminRoutes)
-app.use('/barbero', barberoRoutes)
-app.use(generalRoutes)
+  res.send('Todo ')
 })
 
 
