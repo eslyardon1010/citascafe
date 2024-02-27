473ce4bf0abe0a20bea53b4a18267feef3422ce4
eslyardon1010 <es_ardons@unicah.edu>
2024-02-27 15:51:00 -0600

    commit

--- routes/general.js
+++ routes/general.js
@@ -1,5 +1,6 @@
-const express = require('express')
 const router = require('./routes/routes');
+const express = require('express')
+
 // Controlador
 const controladorGeneral = require('../controllers/general')
 
